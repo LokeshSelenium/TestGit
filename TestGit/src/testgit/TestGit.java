a4662ccf59943110c5eba1e170ec747b7f398060
Sharma <081534@NTTDATA.COM>
2018-10-09 13:06:27 +0530

    TestGit

---
+++ TestGit/src/testgit/TestGit.java
@@ -0,0 +1,17 @@
+package testgit;
+
+public class TestGit
+{
+	public static void main(String[] args) 
+	{
+		int a=20,b=30,c;
+		c=a+b;
+		System.out.println(c);
+		if(c==30)
+		{
+			System.out.println("Sum is 50");
+		}
+
+	}
+
+}
